9a51fa4733b080d7d994fd0b42e920081560551d
mn476 <tas268@cornell.edu>
2023-03-03 19:06:47 -0500

    Initial Commit

---
+++ project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by 2090KM on 3/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
